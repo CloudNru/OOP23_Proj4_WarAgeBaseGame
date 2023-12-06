15177b370659ee576566819287d94da468dae532
Doho-K <kimdohwa2@naver.com>
2023-12-07 00:01:31 +0900

    Images update

--- Assets/Script/EnemyBaseControl.cs
+++ Assets/Script/EnemyBaseControl.cs
@@ -9,6 +9,7 @@ public class EnemyBaseControl : MonoBehaviour
     private int totalGold = 0;
     private int currentLV = 1;
     private int cost = 100;
+    private int spawnState = 1;
     // Update is called once per frame
     private void Start()
     {
@@ -27,18 +28,67 @@ public class EnemyBaseControl : MonoBehaviour
         }
         else if(currentLV == 2)
         {
-            if (GameManager.Instance.GetUsedGold() >= totalGold + cost * (currentLV+1))
+            if (GameManager.Instance.GetUsedGold() >= totalGold + cost * (currentLV-1)&&spawnState == 1)
             {
                 unitFactory.CreateMonster("FirstStudent", awayBaseVec, true);
                 totalGold += cost * currentLV;
+                spawnState = Random.Range(1, 2);
             }
+            else if(GameManager.Instance.GetUsedGold() >= totalGold + cost * (currentLV ) && spawnState == 2)
+            {
+                unitFactory.CreateMonster("FirstStudent", awayBaseVec, true);
+                totalGold += cost * (currentLV - 1);
+                spawnState = Random.Range(1, 2);
+            }
+
         }
         else if(currentLV == 3)
         {
-
+            if (GameManager.Instance.GetUsedGold() >= totalGold + cost * (currentLV-2) && spawnState == 1)
+            {
+                unitFactory.CreateMonster("FirstStudent", awayBaseVec, true);
+                totalGold += cost * (currentLV - 2);
+                spawnState = Random.Range(1, 3);
+            }
+            else if (GameManager.Instance.GetUsedGold() >= totalGold + cost * (currentLV -1 ) && spawnState == 2)
+            {
+                unitFactory.CreateMonster("FirstStudent", awayBaseVec, true);
+                totalGold += cost * (currentLV - 1);
+                spawnState = Random.Range(1, 3);
+            }
+            else if (GameManager.Instance.GetUsedGold() >= totalGold + cost * (currentLV) && spawnState == 3)
+            {
+                unitFactory.CreateMonster("FirstStudent", awayBaseVec, true);
+                totalGold += cost * currentLV;
+                spawnState = Random.Range(1, 3);
+            }
         }
         else if(currentLV == 4)
         {
+            if (GameManager.Instance.GetUsedGold() >= totalGold + cost * (currentLV-3) && spawnState == 1)
+            {
+                unitFactory.CreateMonster("FirstStudent", awayBaseVec, true);
+                totalGold += cost * (currentLV-3);
+                spawnState = Random.Range(1, 4);
+            }
+            else if (GameManager.Instance.GetUsedGold() >= totalGold + cost * (currentLV-2) && spawnState == 2)
+            {
+                unitFactory.CreateMonster("FirstStudent", awayBaseVec, true);
+                totalGold += cost * (currentLV - 2);
+                spawnState = Random.Range(1, 4);
+            }
+            else if (GameManager.Instance.GetUsedGold() >= totalGold + cost * (currentLV-1) && spawnState == 3)
+            {
+                unitFactory.CreateMonster("FirstStudent", awayBaseVec, true);
+                totalGold += cost * (currentLV - 1);
+                spawnState = Random.Range(1, 4);
+            }
+            else if (GameManager.Instance.GetUsedGold() >= totalGold + cost * (currentLV) && spawnState == 4)
+            {
+                unitFactory.CreateMonster("FirstStudent", awayBaseVec, true);
+                totalGold += cost * currentLV;
+                spawnState = Random.Range(1, 4);
+            }
 
         }
         
